acad33d77a2c53ad83f5637007cb5ce8e63c6c49
Mateis CP <mateuscandicocp@gmail.com>
2022-03-11 12:24:13 -0300

    cursophp

---
+++ 16-while-dowhile.php
@@ -0,0 +1,17 @@
+<?php
+//Whie & do while
+
+$cont = 1;
+
+while ($cont <= 10):
+    echo "Contador é $cont <br>";
+    $cont++;
+endwhile;
+
+echo "<hr>";
+
+$cont1 = 1; 
+do{
+    echo "Contador é $cont1 <br>";
+    $cont1++;
+} while ($cont1 <= 10);
---
+++ 17-for-foreach.php
@@ -0,0 +1,4 @@
+<?php
+for ($cont = 0; $cont <=10; $cont++):
+    echo "O contador é $cont <br>";
+endfor;
