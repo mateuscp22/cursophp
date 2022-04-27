5a4e3c00d577e07dcd9e799d4e571b6e51ea885a
Mateus CP <mateuscandicocp@gmail.com>
2022-04-27 15:06:33 -0300

    cursophp

--- 22-formularios/index.php
+++ 22-formularios/index.php
@@ -7,13 +7,64 @@
     <title>Document</title>
 </head>
 <body>
-    <!-- ><form action="dados.php" method="POST"><-->
-    <form action="dados.php" method="GET">
-        Nome: <input type="text" name="nome">
-        Email: <input type="email" name="email">
-        <button type="submit">Enviar</button>
+<?php
+/* Validações
+Funções (filter_input - filter_var)
+FILTER_VALIDATE_INT
+FILTER_VALIDATE_EMAIL
+FILTER_VALIDATE_FLOAT
+FILTER_VALIDATE_IP
+FILTER_VALIDATE_URL
+*/
+?>
+
+<?php
+if(isset($_POST['enviar-formulario'])):
+    //Array de erros
+    $erros = array(); 
+    
+    //Validações
+    if(!$idade = filter_input(INPUT_POST, 'idade', FILTER_VALIDATE_INT)):
+        $erros[] = "Idade precisa ser um numero inteiro";
+    endif;
+
+    if(!$email = filter_input(INPUT_POST, 'email', FILTER_VALIDATE_EMAIL)):
+        $erros[] = "Email invalido";
+    endif;
+
+    if(!$peso = filter_input(INPUT_POST, 'peso', FILTER_VALIDATE_FLOAT)):
+        $erros[] = "Peso precisa ser float";
+    endif;
+
+    if(!$ip = filter_input(INPUT_POST, 'ip', FILTER_VALIDATE_IP)):
+        $erros[] = "IP invalido";
+    endif;
+
+    if(!$url = filter_input(INPUT_POST, 'url', FILTER_VALIDATE_URL)):
+        $erros[] = "URL invalida";
+    endif;
+
+    //Exibindo mensagens de erro
+    if(!empty($erros)):
+        foreach($erros as $erro):
+            echo "<li> $erro";
+        endforeach;
+    else:
+        echo "Dados Corretos";
+    endif;
+endif;
+
+?>
+    <form action="<?PHP echo $_SERVER['PHP_SELF']; ?>" method="POST">
+    <!-- ><form action="dados.php" method="GET"><-->
+        Idade: <input type="text" name="idade"><br>
+        Email: <input type="email" name="email"><br>
+        Peso: <input type="text" name="peso"><br>
+        IP: <input type="text" name="ip"><br>
+        URL: <input type="text" name="url"><br>
+        <button type="submit" name="enviar-formulario">Enviar</button><br>
     </form>
 
-    <a href="dados.php?idade=25&sobrenome=Santos">Enviar dados</a>
+    <!-- ><a href="dados.php?idade=25&sobrenome=Santos">Enviar dados</a><-->
 </body>
 </html>
