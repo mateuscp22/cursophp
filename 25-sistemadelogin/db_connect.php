9260540735b6eb183fc01fee8ed87255ed5fab42
Mateus CP <56638915+mateuscp22@users.noreply.github.com>
2022-07-20 18:43:04 -0300

    teste2

--- 25-sistemadelogin/db_connect.php
+++ 25-sistemadelogin/db_connect.php
@@ -7,8 +7,6 @@ $db_name = "sistemalogin";
 
 $connect = mysqli_connect($servername, $username, $password, $db_name);
 
-if(mysqli_connect_error()):
-    echo "Falha na conexão".mysqli_connect_error();
+if (mysqli_connect_error()) :
+    echo "Falha na conexão" . mysqli_connect_error();
 endif;
-
-ola
